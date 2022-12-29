f77076a4c1c7ae1e9c200e2e49d12ca927b3c688
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-12-29 16:39:54 +0100

    fix: Ejercicio6

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,10 +1,18 @@
-
 /**
  * Ejercicio6.java
  *
  * @version: 29/12/2022
  * @author: Juan Manuel Gutiérrez
  *          https://github.com/Juanma-Gutierrez
+ */
+/*
+ * Implementa el control de acceso al área restringida de un programa. Se debe
+ * pedir un nombre de usuario y una contraseña. Si el usuario introduce los
+ * datos correctamente, el programa dirá “Ha accedido al área restringida”. El
+ * usuario tendrá un máximo de 3 oportunidades. Si se agotan las oportunidades
+ * el programa dirá “Lo siento, no tiene acceso al área restringida”. Los
+ * nombres de usuario con sus correspondientes contraseñas deben estar
+ * almacenados en una estructura de la clase HashMap.
  */
 
 import java.util.HashMap;
