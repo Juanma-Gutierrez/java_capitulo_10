d807c871703f4285edcdf52f7c7799eea3171f7b
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-12-29 17:13:56 +0100

    feat: Ejercicio7

---
+++ Ejercicio7.java
@@ -0,0 +1,45 @@
+
+/**
+ * Ejercicio7.java
+ *
+ * @version: 29/12/2022
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+/*
+ * La máquina Eurocoin genera una moneda de curso legal cada vez que se pulsa
+ * un botón siguiendo la siguiente pauta: o bien coincide el valor con la moneda
+ * anteriormente generada - 1 céntimo, 2 céntimos, 5 céntimos, 10 céntimos, 25
+ * céntimos, 50 céntimos, 1 euro o 2 euros - o bien coincide la posición – cara
+ * o cruz. Simula, mediante un programa, la generación de 6 monedas aleatorias
+ * siguiendo la pauta correcta. Cada moneda generada debe ser una instancia de
+ * la clase Moneda y la secuencia se debe ir almacenando en una lista.
+ */
+import java.util.ArrayList;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        // Var declarations
+        ArrayList<Moneda> list = new ArrayList<Moneda>();
+        Moneda coin;
+        int tries = 10;
+
+        // Var init
+        coin = new Moneda();
+
+        list.add(coin);
+
+        do {
+            coin = new Moneda();
+            if (list.get(list.size() - 1).getValue() == coin.getValue() ||
+                    list.get(list.size() - 1).getHead().equals(coin.getHead())) {
+                list.add(coin);
+                tries--;
+            }
+        } while (tries > 0);
+
+        for (Moneda coinAux : list) {
+            System.out.println(coinAux);
+        }
+    }
+}
---
+++ Moneda.java
@@ -0,0 +1,81 @@
+/**
+ * Moneda.java
+ *
+ * @version: 29/12/2022
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class Moneda {
+    private double value;
+    private String head;
+
+    public Moneda() {
+        int coin;
+        int head;
+
+        coin = (int) (Math.random() * 8);
+        switch (coin) {
+            case 0:
+                this.value = 0.01;
+                break;
+            case 1:
+                this.value = 0.02;
+                break;
+            case 2:
+                this.value = 0.05;
+                break;
+            case 3:
+                this.value = 0.10;
+                break;
+            case 4:
+                this.value = 0.20;
+                break;
+            case 5:
+                this.value = 0.50;
+                break;
+            case 6:
+                this.value = 1;
+                break;
+            case 7:
+                this.value = 2;
+                break;
+        }
+        head = (int) (Math.random() * 2);
+        switch (head) {
+            case 0:
+                this.head = "cara";
+                break;
+            case 1:
+                this.head = "cruz";
+                break;
+        }
+    }
+
+    public String toString() {
+        String res;
+        double value = this.value;
+        if (value < 1)
+            res = this.value + " céntimos - ";
+        else
+            res = this.value + " euro - ";
+        res += this.head;
+        return (res);
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getHead() {
+        return this.head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+}
